78be713c7300883867faeefceb792509bc4639bf
Francisco Acuña <facuna490@gmail.com>
2023-11-22 14:10:29 -0600

    Facebook clone with functional buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+const botonInicio = document.getElementById("boton");
+const botonCrear = document.getElementById("crear");
+
+
+botonInicio.addEventListener('click',() => {
+  alert("Lo siento este sitio sigue en construcción")
+});
+
+botonCrear.addEventListener('click', () => {
+  alert("Lo seinto este sitio sigue en construcción")
+})
+
+
+
